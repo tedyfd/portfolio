212edd4b7aee72faacff6a84547e8de3c17f0411
Tedy Fernando <teddyfernando45@gmail.com>
2024-08-18 12:39:33 +0700

    grid system

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="full-container">
+            <div className="title-page">
+              <h1 className='sub-title'>
+                Tedy Fernando  
+              </h1>
+              <h1 className='title'>
+                Web Developer
+              </h1>
+            </div>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <div className="vertical-center">
+              < h1>Project 1</h1>
+            </div>
+          </div>
+          <div className="col">
+            <div className="middle">
+              <p className='subtitle'>Description 2</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <div className="vertical-center">
+              < h1>Project 1</h1>
+            </div>
+          </div>
+          <div className="col">
+            <div className="middle">
+              <p className='subtitle'>Description 2</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
